6a423f35c645bacd0f40b5b46aaea0cde4c6b394
anhkhoa706 <anhkhoadeptrai706@gmail.com>
2024-12-25 13:44:14 +0800

    Modify tracking pynb

--- track_demo.sh
+++ track_demo.sh
@@ -1,2 +1,2 @@
 # BotSORT 
-python tracking/track.py --source "videos/32_1F_2024_9_2_part2.mp4" --tracking-method botsort --yolo-model yolov10x --classes 0 --save
+python tracking/track.py --source videos/32_1F_2024_9_2_part2.mp4 --tracking-method botsort --yolo-model weights/yolov10x --classes 0 --save
